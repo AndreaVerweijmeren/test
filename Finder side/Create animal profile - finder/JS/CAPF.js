8befeba59c589ae94fc185766fdcd43ee92957a3
AndreaVerweijmeren <61025668+AndreaVerweijmeren@users.noreply.github.com>
2020-04-02 19:40:37 +0200

    test

--- Finder side/Create animal profile - finder/JS/CAPF.js
+++ Finder side/Create animal profile - finder/JS/CAPF.js
@@ -1,5 +1,21 @@
 // JavaScript Document
 
+function previewFile(){
+  var preview = document.querySelector('img'); //selects the query named img
+  var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file); //reads the data as a URL
+  } 
+}
+
+  previewFile();  //calls the function named previewFile()
+
 /* BOOTSTRAP DROPDOWN MENU - Update selected item text and image */
 $(".dropdown-menu li a").click(function () {
     var selText = $(this).text();
